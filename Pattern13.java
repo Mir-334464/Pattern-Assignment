f7cafc1aea5f1c4c10645840c881d7179c03a74d
Mir-334464 <106953464+Mir-334464@users.noreply.github.com>
2022-07-01 13:42:39 +0530

    Add files via upload

---
+++ Pattern13.java
@@ -0,0 +1,24 @@
+ class Pattern13
+   {
+
+	public static void main(String[] args) 
+
+           {
+		
+		 for (int i = 1; i <= 5; i++)
+              {
+            for (int j = 5; j > i; j--)
+               {
+                System.out.print(" ");
+                  }
+
+            int x = 1;
+            for (int k = 1; k <= i; k++)
+            {
+                System.out.print(x+ " ");
+                x = x * (i - k) / (k);  //Paskal method
+            }
+            System.out.println();
+        }
+	}
+}
